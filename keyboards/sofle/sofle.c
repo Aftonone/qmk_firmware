eb6b2ceec8905fa7005ea5135b03d371d4eb4336
Aftonone <aftonone@gmail.com>
2026-03-09 00:24:38 -0500

    Working baseline. Code is up to date with all my previous changes.

--- keyboards/sofle/sofle.c
+++ keyboards/sofle/sofle.c
@@ -52,85 +52,131 @@ oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
         return OLED_ROTATION_270;
     }
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_270;
+    }
     return rotation;
 }
 
+/* QMK logo
 static void render_logo(void) {
+
     static const char PROGMEM qmk_logo[] = {
         0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
         0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
         0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
     };
     oled_write_P(qmk_logo, false);
-}
 
-void print_status_narrow(void) {
-    oled_write_P(PSTR("\n\n"), false);
-    switch (get_highest_layer(layer_state)) {
+}
+*/
+
+static void print_slave_status_narrow(void) {
+    oled_write_ln_P(PSTR("LYOUT"), false);
+    switch (get_highest_layer(layer_state|default_layer_state)) {
         case 0:
-            oled_write_ln_P(PSTR("Qwrt"), false);
+            oled_write_ln_P(PSTR("Qwrty"), false);
             break;
         case 1:
-            oled_write_ln_P(PSTR("Clmk"), false);
+            oled_write_ln_P(PSTR("Clmk\n"), false);
+            break;
+        case 2:
+            oled_write_ln_P(PSTR("Game\n"), false);
             break;
         default:
-            oled_write_P(PSTR("Mod\n"), false);
+            oled_write_ln_P(PSTR("Mod\n"), false);
             break;
     }
-    oled_write_P(PSTR("\n\n"), false);
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
         case 0:
         case 1:
-            oled_write_P(PSTR("Base\n"), false);
+            oled_write_ln_P(PSTR("Base\n"), false);
             break;
         case 2:
-            oled_write_P(PSTR("Lower"), false);
+            oled_write_ln_P(PSTR("Base"), false);
             break;
         case 3:
-            oled_write_P(PSTR("Raise"), false);
+            oled_write_ln_P(PSTR("Symbl"), false);
             break;
         case 4:
-            oled_write_P(PSTR("Adjust"), false);
+            oled_write_ln_P(PSTR("Mod"), false);
+            break;
+        case 5:
+            oled_write_ln_P(PSTR("Adjst"), false);
+            break;
+        case 6:
+            oled_write_ln_P(PSTR("Mouse"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
+    oled_write_ln_P(PSTR("-----"), false);
+    oled_write_ln_P(PSTR("WPM"), false);
+
+    uint8_t n = get_current_wpm();
+    char wpm_str[4];
+    wpm_str[3] = '\0';
+    wpm_str[2] = '0' + n % 10;
+    wpm_str[1] = '0' + (n /= 10) % 10;
+    wpm_str[0] = '0' + n / 10;
+
+    oled_write(wpm_str, false);
+}
+
+void print_status_narrow(void) {
+    //oled_write_P(PSTR("\n\n"), false);
+
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
+    oled_write_ln_P(PSTR("CRRCT"), autocorrect_is_enabled());
+    oled_write_ln_P(PSTR("SHIFT"), get_autoshift_state());
 }
 
+// This function is called for every OLED update, and decides which display to render.
 bool oled_task_kb(void) {
-    if (!oled_task_user()) {
-        return false;
-    }
+    //if (!oled_task_user()) {
+    //    return false;
+    //}
     if (is_keyboard_master()) {
         print_status_narrow();
     } else {
-        render_logo();
+        //render_logo();
+        print_slave_status_narrow();
     }
     return true;
 }
-
 #endif
 
+//Set encoder keycodes here.
+// Encoder 0 is left encoder, 1 is right encoder
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) {
         return false;
     }
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
+    if (get_highest_layer(layer_state) == 6) { // Mouse layer
+        if (index == 0) {
+            if (clockwise) {
+                tap_code(KC_PGUP);
+            } else {
+                tap_code(KC_PGDN);
+            }
         }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
+    } else {
+        if (index == 0) {
+            if (clockwise) {
+                tap_code(KC_MPRV);
+            } else {
+                tap_code(KC_MNXT);
+            }
+        } else if (index == 1) {
+            if (clockwise) {
+                tap_code(KC_VOLD);
+            } else {
+                tap_code(KC_VOLU);
+            }
         }
     }
     return true;
