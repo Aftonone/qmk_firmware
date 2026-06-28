4d1b2792fe95183111995dee0f0ef4c34f79b09b
Aftonone <aftonone@gmail.com>
2026-06-28 18:30:38 -0500

    Deleted .json file that prevented building. Added backspace layer tap to right thumb raise key.

--- keyboards/sofle/keymaps/Aftonone/keymap.c
+++ keyboards/sofle/keymaps/Aftonone/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F),    KC_G,                     KC_H,    RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN),  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_MUTE, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTL, TL_LOWR, KC_ENT,      TD(DOT_SPC),  TL_UPPR,  MO(_MOUSE), LGUI(KC_TAB), LALT(KC_SPC)
+                 KC_LGUI,KC_LALT,KC_LCTL, TL_LOWR, KC_ENT,      TD(DOT_SPC),  LT(_RAISE, KC_BSPC),  MO(_MOUSE), LGUI(KC_TAB), LALT(KC_SPC)
 ),
 /*
  * COLEMAK
